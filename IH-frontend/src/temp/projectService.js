94e054cd55295f2cbdf82cbc4aefd259ad6d251c
jyothishjayasan07 <jyothishjayasan@interns.cybersquare.org>
2025-07-30 17:32:24 +0530

    Update projectService.js
    fix api_url

--- IH-frontend/src/temp/projectService.js
+++ IH-frontend/src/temp/projectService.js
@@ -1,4 +1,4 @@
-const Base_url=import.meta.env.VITE_API_URL|| "https://investors-hub-new.onrender.com";
+const API_URL=import.meta.env.VITE_API_URL|| "https://investors-hub-new.onrender.com";
 
 export const fetchProjects = async (token) => {
   const res = await fetch(`${API_URL}/projectofuser`, {
